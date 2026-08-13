b9795568c35e6e2256fc695dec3d6f89d077692f
Cristian O <84862634+Criser2013@users.noreply.github.com>
2026-08-13 20:58:39 +0000

    Pruebas del módulo "Dominios" actualizadas.

--- tests/scripts/utils/test_diccionario.py
+++ tests/scripts/utils/test_diccionario.py
@@ -3,10 +3,12 @@ from app.utils.Diccionario import ver_si_existe_clave
 
 DICCIONARIO = {"clave1": 1, "clave2": 2, "clave3": 3}
 
-@pytest.mark.parametrize("diccionario,clave,respuesta_esperada",[
-    (DICCIONARIO, "clave1", True),
-    (DICCIONARIO, "clave4", False)
-], ids=["test_35", "test_36"])
+
+@pytest.mark.parametrize(
+    "diccionario,clave,respuesta_esperada",
+    [(DICCIONARIO, "clave1", True), (DICCIONARIO, "clave4", False)],
+    ids=["test_35", "test_36"],
+)
 def test_ver_si_existe_clave(diccionario, clave, respuesta_esperada):
     """
     Test para validar que las función "ver_si_existe_clave" retorne True si la clave existe en el diccionario.
